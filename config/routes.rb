e521724be1e0a4029b2d3b47cd3336e2f8fd558c
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-24 11:31:51 +0100

    routes for flags

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Forem::Engine.routes.draw do
   root :to => "forums#index"
-
-  resources :topics, :only => [:new, :create, :index, :show, :destroy] do
+  concern :flaggable do
+    resources :flags, only: [:create, :destroy]
+  end
+  resources :topics, only: [:new, :create, :index, :show, :destroy] do
     resources :posts
   end
   resource :topics do
       get 'fresh_topics', as: :fresh_topics, controller: :topics, action: :fresh_topics
   end
-
-  resources :categories, :only => [:index, :show]
-
+  resources :categories, only: [:index, :show]
   namespace :admin do
     root :to => "base#index"
     resources :groups do
@@ -19,11 +19,9 @@ Forem::Engine.routes.draw do
         end
       end
     end
-
     resources :forums do
       resources :moderators
     end
-
     resources :categories
     resources :topics do
       member do
@@ -32,16 +30,13 @@ Forem::Engine.routes.draw do
         put :toggle_pin
       end
     end
-
     get 'users/autocomplete', :to => "users#autocomplete", :as => "user_autocomplete"
   end
-
   get '/:forum_id/moderation', :to => "moderation#index", :as => :forum_moderator_tools
   # For mass moderation of posts
   put '/:forum_id/moderate/posts', :to => "moderation#posts", :as => :forum_moderate_posts
   # Moderation of a single topic
   put '/:forum_id/topics/:topic_id/moderate', :to => "moderation#topic", :as => :moderate_forum_topic
-
   resources :forums, :only => [:index, :show], :path => "/" do
     resources :topics do
       member do
