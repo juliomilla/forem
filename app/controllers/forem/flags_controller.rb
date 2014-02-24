0659cbec86edb8c2258043c486082f7f296de856
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-24 17:30:12 +0100

    ...

--- app/controllers/forem/flags_controller.rb
+++ app/controllers/forem/flags_controller.rb
@@ -1,16 +1,12 @@
 module Forem
   class FlagsController < Forem::ApplicationController
-
     def create
-
       if params[:post_id]
         flaggable = Post.find(params[:post_id])
       end
-
       @flag = Flag.new
       @flag.forem_flaggable = flaggable
       @flag.user = current_user
-
       if @flag.save
         respond_to do |format|
           format.html { render nothing: true, status: 204, layout: !request.xhr? }
@@ -25,14 +21,12 @@ module Forem
     end
 
     def destroy
-      
       @flag = Flag.find(params[:id])
-
       if @flag.user == current_user
         if @flag.destroy
           respond_to do |format|
             format.html { render nothing: true, status: 204 }
-            format.json { render nothing: true, status: 204}
+            format.json { render nothing: true, status: 204 }
           end
         else
           respond_to do |format|
