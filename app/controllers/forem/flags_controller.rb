5a6277e09e18a82f7da725c66000f1e5d2a98f0f
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-24 14:28:05 +0100

    logic for flags controller

--- app/controllers/forem/flags_controller.rb
+++ app/controllers/forem/flags_controller.rb
@@ -1,17 +1,18 @@
 module Forem
   class FlagsController < Forem::ApplicationController
     def create
-      flaggable = params[:flaggable_type].classify.constantize.find(params[:flaggable_id])
+
+      if params[:post_id]
+        flaggable = Post.find(params[:post_id])
+      end
 
       @flag = Flag.new
       @flag.flaggable = flaggable
       @flag.user = current_user
 
       if @flag.save
-        @flaggable_parent = params[:flaggable_parent].classify.constantize.find(params[:flaggable_parent_id])
-
         respond_to do |format|
-          format.html { render 'show', status: 200, layout: !request.xhr? }
+          format.html { render nothing: true, status: 204, layout: !request.xhr? }
           format.json { render json: @flag, status: 200 }
         end
       else
@@ -21,8 +22,11 @@ module Forem
         end
       end
     end
+
     def destroy
+      
       @flag = Flag.find(params[:id])
+
       if @flag.user == current_user
         if @flag.destroy
           respond_to do |format|
