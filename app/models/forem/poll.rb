1d0c30b16e23cb68c6ad6a0b386dcbaf15feb423
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-27 15:33:00 +0100

    dat params so nested

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -3,5 +3,6 @@ module Forem
     belongs_to :topic
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
+    accepts_nested_attributes_for :poll_options
   end
 end
--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -34,6 +34,7 @@ module Forem
     has_one :poll
 
     accepts_nested_attributes_for :posts
+    accepts_nested_attributes_for :poll
 
     validates :subject, :presence => true
     validates :user, :presence => true
