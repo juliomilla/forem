db98a983ec4dda0f06d2a2220f88a23a1e80b804
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-28 11:58:52 +0100

    asdf

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -1,6 +1,6 @@
 module Forem
   class Poll < ActiveRecord::Base
-    belongs_to :forem_topic
+    belongs_to :topic, foreign_key: :forem_topic_id
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
     # accepts_nested_attributes_for :poll_options
