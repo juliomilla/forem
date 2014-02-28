c34baddfe76ae9f7e9010b6cc0abc0083b63a1fb
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-28 14:36:01 +0100

    such nested attributes acceptance very wow

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -3,6 +3,6 @@ module Forem
     belongs_to :topic, foreign_key: :forem_topic_id
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
-    # accepts_nested_attributes_for :poll_options
+    accepts_nested_attributes_for :poll_options
   end
 end
