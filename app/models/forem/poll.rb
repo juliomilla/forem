6a11a293501f5e3f9c8c0611599eec70dd627d1a
Per Andersen <per.andersen@slagkryssaren.com>
2014-02-28 11:53:39 +0100

    such poll unbugging

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -1,6 +1,6 @@
 module Forem
   class Poll < ActiveRecord::Base
-    belongs_to :topic
+    belongs_to :forem_topic
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
     # accepts_nested_attributes_for :poll_options
