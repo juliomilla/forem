88fa23c76a3bd24c8238cdec38683cd3c806c8dd
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-03 14:01:52 +0100

    dat foreign key b broken

--- app/models/forem/poll_option.rb
+++ app/models/forem/poll_option.rb
@@ -1,6 +1,6 @@
 module Forem
   class PollOption < ActiveRecord::Base
-    belongs_to :poll, foreign_key: :forem_poll_id
+    belongs_to :poll
     validates :description, presence: true
     has_and_belongs_to_many :voting_users, class_name: Forem.user_class.to_s
 
