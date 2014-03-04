5431ecaf468a57ea981825c7e091cf5fa4cd73a3
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-04 13:59:47 +0100

    ..

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -5,7 +5,8 @@ module Forem
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
     accepts_nested_attributes_for :poll_options
-
+    
+    has_many :votes
     has_many :voting_users, through: :votes, class_name: Forem.user_class.to_s
     
     def vote poll_option_id, user
