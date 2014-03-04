f564bb5266e0e4e4279b005f5ebddcadb597b7db
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-04 13:54:46 +0100

    ..

--- app/models/forem/vote.rb
+++ app/models/forem/vote.rb
@@ -1,7 +1,7 @@
 module Forem
   class Vote < ActiveRecord::Base
     belongs_to :poll
-    belongs_to :user
+    belongs_to :user, class_name: Forem.user_class.to_s
 
     validates :poll_id, uniqueness: { scope: :user_id }
   end
