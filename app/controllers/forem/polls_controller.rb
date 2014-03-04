f12a735876ec566ba71865f72142e189b249b03d
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-04 10:11:08 +0100

    fixed broken relation and added some translation

--- app/controllers/forem/polls_controller.rb
+++ app/controllers/forem/polls_controller.rb
@@ -3,11 +3,11 @@ module Forem
     before_filter :authenticate_forem_user
 
     def vote
-      poll_option = PollOption.find(params[:poll_option_id])
+      poll_option = Forem::PollOption.find(params[:poll_option_id])
       poll_option.votes = poll_option.votes + 1
-      poll_option.users << forem_user
+      poll_option.poll.voting_users << forem_user
 
-      if poll_option.save
+      if poll_option.save && poll_option.poll.save
         respond_to do |format|
           format.js { render nothing: true, status: 204}
           format.html { render nothing: true, status: 204}
--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -1,6 +1,7 @@
 module Forem
   class Poll < ActiveRecord::Base
-    belongs_to :topic, foreign_key: :forem_topic_id
+
+    belongs_to :topic
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
     accepts_nested_attributes_for :poll_options
