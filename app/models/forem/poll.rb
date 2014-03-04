62c851c4ab7c1956600cc069f7c7f2a63cdafa4f
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-04 14:01:51 +0100

    dat source

--- app/models/forem/poll.rb
+++ app/models/forem/poll.rb
@@ -5,9 +5,9 @@ module Forem
     has_many :poll_options, dependent: :destroy
     validates :question, presence: true
     accepts_nested_attributes_for :poll_options
-    
+
     has_many :votes
-    has_many :voting_users, through: :votes, class_name: Forem.user_class.to_s
+    has_many :voting_users, through: :votes, class_name: Forem.user_class.to_s, source: :user
     
     def vote poll_option_id, user
       opt = self.poll_options.find(poll_option_id)
