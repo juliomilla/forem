a592a70e74298200dcc57028b93efb0447f9cf3e
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-05 10:19:15 +0100

    fucking validations

--- app/models/forem/vote.rb
+++ app/models/forem/vote.rb
@@ -3,6 +3,7 @@ module Forem
     belongs_to :poll
     belongs_to :user, class_name: Forem.user_class.to_s
 
+    validates_uniqueness_of :poll_id, scope: :user_id
     # validates :user_id, uniqueness: { scope: :poll_id }
   end
 end
