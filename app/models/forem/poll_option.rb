81a592bb2f915729910253712f90ebc44e05529a
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-10 10:08:46 +0100

    why is order randooom?

--- app/models/forem/poll_option.rb
+++ app/models/forem/poll_option.rb
@@ -4,6 +4,8 @@ module Forem
     belongs_to :poll
     validates :description, presence: true
 
+    default_scope { order('id DESC') }
+
     def votes_percentage_of_total
       tot = self.poll.poll_options.pluck(:votes).inject(0) {|acc, elem| acc + elem}
       if !tot.nil? && !self.votes.nil? && self.votes > 0 && tot > 0
