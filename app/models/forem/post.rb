368ae6cf0ccf5027d8dc17bd7e34ef3a73ab2ead
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-17 14:09:16 +0100

    .

--- app/models/forem/post.rb
+++ app/models/forem/post.rb
@@ -32,7 +32,7 @@ module Forem
 
     validates :text, :presence => true
 
-    has_one :forum, through: :topic#, counter_cache: true
+    has_one :forum, through: :topic, counter_cache: true
     # delegate :forum, :to => :topic
 
     # has_one :forum, through: :topic, counter_cache: :posts_count
