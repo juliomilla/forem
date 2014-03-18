078ad5501c3a8a0d138fe030922ebe937a0c63b7
Per Andersen <per.andersen@slagkryssaren.com>
2014-03-18 13:37:17 +0100

    yolo

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 Kaminari.configure do |config|
   # config.default_per_page = 5
   # config.max_per_page = nil
-  # config.window = 4
-  config.outer_window = 3
+  config.window = 2
+  config.outer_window = 2
   # config.left = 0
   # config.right = 0
   # config.page_method_name = :page
