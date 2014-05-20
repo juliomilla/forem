de5dd9203e04dc8478a102524e2e7c6ee9372a39
Per Andersen <per.andersen@slagkryssaren.com>
2014-05-20 15:57:08 +0200

    more scopes

--- app/models/forem/topic.rb
+++ app/models/forem/topic.rb
@@ -44,7 +44,7 @@ module Forem
 
     scope :with_user, -> { includes :user }
     scope :with_last_post, -> { includes last_post: {user: {}, topic: {}, forum: {} } }
-
+    scope :with_poll, -> { includes :poll }
     class << self
       def visible
         where(:hidden => false)
