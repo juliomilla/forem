e6f9a2db24aeb1834fe3d4235183245f818268d8
Per Andersen <per.andersen@slagkryssaren.com>
2014-05-22 12:59:22 +0200

    fuck you forem, really

--- app/controllers/forem/admin/users_controller.rb
+++ app/controllers/forem/admin/users_controller.rb
@@ -4,7 +4,7 @@ module Forem
       users = Forem.user_class.forem_autocomplete(params[:term])
 
       users_hash = users.map do |user|
-        { :id => user.id, :identifier => user.send(Forem.autocomplete_field), name: user.send(:name) }
+        { :id => user.id, :identifier => user.send(Forem.autocomplete_field)}
       end
       render :json => users_hash
     end
