62e478340dbeec00f5ea1c4ed4dca88b86e31cfd
Per Andersen <per.andersen@slagkryssaren.com>
2014-08-01 16:18:48 +0200

    security fix

--- app/controllers/forem/polls_controller.rb
+++ app/controllers/forem/polls_controller.rb
@@ -8,27 +8,25 @@ module Forem
           format.js { render nothing: true, status: 401 }
           format.html { render nothing: true, status: 401 }
         end
-      end
-
-      poll_option = Forem::PollOption.find(params[:poll_option_id])
-      poll_option.votes = poll_option.votes + 1
-      poll_option.poll.voting_users << forem_user
-
-      begin
-        saved = poll_option.save
-      rescue
-        saved = false
-      end
-
-      if saved
-        respond_to do |format|
-          format.js { render nothing: true, status: 204}
-          format.html { render nothing: true, status: 204}
-        end
       else
-        respond_to do |format|
-          format.js { render nothing: true, status: 400 }
-          format.html { render nothing: true, status: 400 }
+        poll_option = Forem::PollOption.find(params[:poll_option_id])
+        poll_option.votes = poll_option.votes + 1
+        poll_option.poll.voting_users << forem_user
+        begin
+          saved = poll_option.save
+        rescue
+          saved = false
+        end
+        if saved
+          respond_to do |format|
+            format.js { render nothing: true, status: 204}
+            format.html { render nothing: true, status: 204}
+          end
+        else
+          respond_to do |format|
+            format.js { render nothing: true, status: 400 }
+            format.html { render nothing: true, status: 400 }
+          end
         end
       end
     end
